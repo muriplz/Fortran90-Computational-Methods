340bbe65b4aa2d5931f6234318cbabfdde727f7e
muriplz <46825658+muriplz@users.noreply.github.com>
2022-04-19 12:49:57 +0200

    fixed Secant Method condition

--- FunctionRoots/SecantMethod.f90
+++ FunctionRoots/SecantMethod.f90
@@ -6,7 +6,7 @@ real*8 function Secant(x0,x1,f,errorY)
 real*8 :: x0,x1,slope,errorY
 external :: f
 
-do while (abs(f(Secant))>errorY)
+do while (abs(f(x1))>errorY)
   slope=(f(x1)-f(x0))/(x1-x0)
   x1=-(f(x1)-slope*x1)/slope
 end do
